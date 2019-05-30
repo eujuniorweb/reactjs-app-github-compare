7e0884b84a48a8a9f2100fa2a2ca9526629473b1
jsdeveloper <jros2.silva@gmail.com>
2019-05-30 00:56:30 -0300

    Buscando repositorios da API e formatando data

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Container, Repository } from './styles';
 
 const List = ({ repositories }) => (
   <Container>
     {repositories.map(repository => (
-      <Repository>
+      <Repository key={repository.id}>
         <header>
           <img src={repository.owner.avatar_url} alt={repository.owner.login} />
-          <strong>repository.name</strong>
+          <strong>{repository.name}</strong>
           <small>{repository.owner.login}</small>
         </header>
         <ul>
@@ -25,7 +26,7 @@ const List = ({ repositories }) => (
             <small>issues</small>
           </li>
           <li>
-            {repository.pushed_at}
+            {repository.lastCommit}
             <small>last commit</small>
           </li>
         </ul>
@@ -33,5 +34,20 @@ const List = ({ repositories }) => (
     ))}
   </Container>
 );
-
+List.propTypes = {
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      owner: PropTypes.shape({
+        login: PropTypes.string,
+        avatar_url: PropTypes.string,
+      }),
+      stargazers_count: PropTypes.number,
+      forks_count: PropTypes.number,
+      open_issues_count: PropTypes.number,
+      pushed_at: PropTypes.string,
+    }),
+  ).isRequired,
+};
 export default List;
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,19 +1,43 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import { Container, Form } from './styles';
 import logo from '../../assets/logo.png';
 import List from '../../components/List';
+import api from '../../services/api';
 
 export default class Main extends Component {
   state = {
+    input: '',
     repositories: [],
+  };
+
+  handleAddRepository = async (event) => {
+    event.preventDefault();
+    try {
+      const { data: repository } = await api.get(`/repos/${this.state.input}`);
+      repository.lastCommit = moment(repository.pushed_at).fromNow();
+      this.setState({
+        input: '',
+        repositories: [...this.state.repositories, repository],
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   render() {
     return (
       <Container>
         <img src={logo} alt="Git Compare" />
-        <Form action="">
-          <input type="text" placeholder="usuário/repositorio" />
+        <Form onSubmit={this.handleAddRepository} action="">
+          <input
+            type="text"
+            placeholder="usuário/repositorio"
+            value={this.state.input}
+            onChange={(event) => {
+              this.setState({ input: event.target.value });
+            }}
+          />
           <button type="submit">OK</button>
         </Form>
         <List repositories={this.state.repositories} />
